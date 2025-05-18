971c79df7e8a48896319688018fee3ecbe0473ec
Rodeobe <43032396+Rodeobe@users.noreply.github.com>
2025-05-18 20:20:40 +0200

    invullen

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "DentiVentures - Specialists in jaw pain solutions";
+export const SITE_DESCRIPTION = "Together we set up your TMJ Pain idea";
